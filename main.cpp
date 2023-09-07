bffb7aff966839512bd1d518af0259ce32920dc8
Lyubitskiy Maxim <root@DESKTOP-BK46MSC>
2023-09-07 17:17:55 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdlib>
+
+int * genRandArray(int size, int maxValue)
+{
+    int * arr = new int[size+1];
+    arr[0] = size;
+    for (int i = 1; i < size + 1; i++)
+    {
+        arr[i] = rand() % maxValue;
+    }
+    
+    return arr;
+}
+
+int ** genRandMatrix(int size, int maxValue)
+{
+    int ** arr = new int*[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = genRandArray(rand()%10, maxValue);
+    }
+
+    return arr;
+}
+
+void print(int * arr)
+{
+    int size = arr[0];
+    std::cout << size << ": ";
+    for (int i = 1; i < size + 1; i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
+void printMatrix(int ** arr, int size)
+{
+    std::cout << "size matrix: " << size << '\n';
+    for(int i = 0; i < size; i++)
+    {
+        print(arr[i]);
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    int size = rand()%10;
+    int maxValue = 100;
+    int* arr = genRandArray(size, maxValue);
+    print(arr);
+    delete [] arr;
+
+    std::cout << "---------------\n";
+
+    size=rand()%10;
+    int** matrix = genRandMatrix(size, maxValue);
+    printMatrix(matrix, size);
+    for (int i = 0; i < size; i++)
+    {
+        delete [] matrix[i];
+    }
+    delete [] matrix;
+
+    return 0;
+}
